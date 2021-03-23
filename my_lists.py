e335976d37f9174194f32f77f370f339fadd08d2
devadikhagesh <devadi.khagesh@gmail.com>
2021-03-23 12:29:27 +0530

    adding a code file

---
+++ my_lists.py
@@ -0,0 +1,11 @@
+list = [1,2,3,4,5,3]
+list.reverse()
+print("list data after reversing it:", list)
+
+list1 = [3,23,34,656,4,24,24]
+list1.sort()
+print("sorted list is :",list1)
+
+list3 = []
+list3 = list1.copy()
+print("New list is", list3)
